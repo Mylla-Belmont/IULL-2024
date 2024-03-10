4e91501ac9706d361ee311088bb68d623327af19
Mylla-belmont <camilabelmont.alu@gmail.com>
2024-03-10 13:58:25 -0300

    adicionando responsividade nas imagens

--- IULL-projects/src/app/app.component.ts
+++ IULL-projects/src/app/app.component.ts
@@ -2,13 +2,22 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
+import { IMAGE_CONFIG, NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  providers: [],
+  providers: [
+    {
+      provide: IMAGE_CONFIG,
+      useValue: {
+        breakpoints: [16, 48, 96, 128, 384, 640, 750, 828, 1080, 1200, 1920]
+      }
+    },
+  ],
   imports: [
     RouterOutlet,
+    NgOptimizedImage,
     HomeComponent
   ],
   templateUrl: './app.component.html',
